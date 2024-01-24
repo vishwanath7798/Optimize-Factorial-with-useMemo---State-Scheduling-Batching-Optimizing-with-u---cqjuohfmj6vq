62bc06a6b1931126f82ef1049a4b8c25c644b071
vishwanath s patil <126999436+vishwanath7798@users.noreply.github.com>
2024-01-24 21:27:18 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,21 @@
 import { useState } from 'react';
 import FactorialComponent from '../components/FactorialComponent';
 export default function Home() {
-  
-     
+  const [number, setNumber] = useState(5)
+
   return (
     <div>
-        <h1>Welcome to Next.js!</h1>
-        <section>
-                <h2>Factorial Calculation</h2>
-                <input 
-                    type="number" 
-                    
-                   } 
-                />
-       
-                <FactorialComponent  />
-            </section>
+      <h1>Welcome to Next.js!</h1>
+      <section>
+        <h2>Factorial Calculation</h2>
+        <input
+          type="number"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+
+        <FactorialComponent number={number} />
+      </section>
     </div>
-);
+  );
 }

91774a2076eb2aa6f784102116cfc7a044b63616
vishwanath7798 <vishwanathspatil07@gmail.com>
2024-02-20 23:04:18 +0530

    stack

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
+// "use client"
+// import { useState } from 'react';
+// import FactorialComponent from '../components/FactorialComponent';
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Welcome to Next.js!</h1>
+//       <section>
+//         <h2>Factorial Calculation</h2>
+//         <input
+//           type="number"
+//         />
+
+//         <FactorialComponent/>
+//       </section>
+//     </div>
+//   );
+// }
+
+
 "use client"
 import { useState } from 'react';
 import FactorialComponent from '../components/FactorialComponent';
 export default function Home() {
-  const [number, setNumber] = useState(5)
-
+  const[number,setNumber]=useState(5)
   return (
     <div>
       <h1>Welcome to Next.js!</h1>
@@ -12,11 +31,39 @@ export default function Home() {
         <input
           type="number"
           value={number}
-          onChange={(e) => setNumber(e.target.value)}
+          onChange={(e)=> setNumber(e.target.value)}
         />
 
-        <FactorialComponent number={number} />
+        <FactorialComponent number={number}/>
       </section>
     </div>
   );
 }
+
+
+
+
+
+
+// "use client"
+// import { useState } from 'react';
+// import FactorialComponent from '../components/FactorialComponent';
+// export default function Home() {
+//   const [number, setNumber] = useState(5)
+
+//   return (
+//     <div>
+//       <h1>Welcome to Next.js!</h1>
+//       <section>
+//         <h2>Factorial Calculation</h2>
+//         <input
+//           type="number"
+//           value={number}
+//           onChange={(e) => setNumber(e.target.value)}
+//         />
+
+//         <FactorialComponent number={number} />
+//       </section>
+//     </div>
+//   );
+// }
